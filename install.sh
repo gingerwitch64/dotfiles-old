cb6e63ad751326b02fb817c7df14634c2d0a043a
Nola G <90254024+gingerwitch64@users.noreply.github.com>
2022-10-03 19:18:46 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Config install script
+# Inspiration credit to aristonl at:
+# https://github.com/aristonl/dotfiles/blob/main/install
+
+echo "Creating directories and copying configs..."
+mkdir ~/.config ~/.config/{alacritty,nvim}
+cp ./alacritty.yml ~/.config/alacritty
+cp ./init.vim ~/.config/init.vim
+
+echo "Installing vim-plug..."
+
+# Credit to Ariston for this next part
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim -c 'PlugInstall' \
+	 -c 'qa!'
+
+echo "...Done!"
